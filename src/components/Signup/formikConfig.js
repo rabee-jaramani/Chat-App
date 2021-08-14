431677970a9c3120f0b48f66671f130a71faee37
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-08-14 14:47:19 +0400

    Update formikConfig.js

--- src/components/Signup/formikConfig.js
+++ src/components/Signup/formikConfig.js
@@ -9,7 +9,7 @@ export const defaultValues={
 
 export const validationSchema=Yup.object().shape({
     email:Yup.string().email('Invalid Email').required('Required'),
-    password:Yup.string().required('Required').min(2,'Must be at least 2 charecters (:'),
+    password:Yup.string().required('Required').min(8,'Must be at least 8 charecters (:'),
     verifyPassword:Yup.string().required('Required').oneOf([Yup.ref('password'),null],'Passwords are not same'),
     userName:Yup.string().required('Required').matches(/^\S*$/,'No spaces').min(3,'Must be at least 3 charecters')
 })
