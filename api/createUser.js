b462d25a63a62a75363f78187fbc6fd083b94afd
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-09-12 15:18:35 +0400

    Update createUser.js

--- api/createUser.js
+++ api/createUser.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // backend method to create user on chat engine
 const createUser=async(req,res)=>{
     const {userId,userName}=req.body;
-    axios.post('https://api.chatengine.io/projects/people/',
+    await axios.post('https://api.chatengine.io/projects/people/',
     // user name and secret in chat engine must be provided and private key in the header
     {username:userName,secret:userId},
     {headers:{'Private-key':'0c941f5d-62f0-4a20-b80b-4c07c4d814c3'}}
